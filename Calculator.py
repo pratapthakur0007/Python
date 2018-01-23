1f2af8a6110247fcd4a6fe841614cefca5d3ce7b
Pratap Singh <pratapthakur0007@gmail.com>
2018-01-23 11:39:05 +0530

    Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,47 @@
+Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:04:45) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 2 + 2   # Addition
+4
+>>> 50 - 5*6 # Subtraction
+20
+>>> (50 - 5*6) / 4
+5.0
+>>> 8 / 5 # division always returns a floating point number
+1.6
+>>> 
+>>> 
+>>> 
+>>> 5 ** 2   # squared
+25
+>>> 2 ** 7   # 2 to the power of 7
+128
+>>> 
+>>> 
+>>> width = 20
+>>> height = 5 * 9
+>>> 
+>>> width * height
+900
+>>> 
+>>> n  #try to access an undefined variable
+Traceback (most recent call last):
+  File "<pyshell#16>", line 1, in <module>
+    n  #try to access an undefined variable
+NameError: name 'n' is not defined
+>>> 
+>>> 
+>>> 4 * 3.75 - 1
+14.0
+>>> 
+>>> 
+>>> 
+>>> tax = 12.5 / 100
+>>> price = 100.50
+>>> price * tax
+12.5625
+>>> price + _
+113.0625
+>>> 
+>>> round(_, 2)
+113.06
+>>> 
